436dcf477de3d13ff12a8bcc971e37e70393d002
Lab Luppes <labs@luppes.com>
2025-02-06 16:47:27 -0600

    test output

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -4,3 +4,11 @@ echo "hello $1"
 echo "RESOURCE_GROUP_NAME is $2"
 echo "time=$(date)" >> $GITHUB_OUTPUT
 echo "RESOURCE_GROUP_NAME=$2" >> $GITHUB_OUTPUT
+
+env | while IFS= read -r line; do
+  value=${line#*=}
+  name=${line%%=*}
+  echo "V: $value"
+  echo "N: $name"
+  echo "$name=$value" >> $GITHUB_OUTPUT
+done
